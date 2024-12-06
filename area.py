d89d092f86ad248969fbc8686c20df803b99e903
Akhi-la-Madhu <159412941+Akhi-la-Madhu@users.noreply.github.com>
2024-12-06 10:30:40 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,6 @@
+def area(a):
+         result=3.14*a*a
+         return result
+def perimeter(a):
+    result=2*3.14*a
+    return result
---
+++ circlearea.py
@@ -0,0 +1,6 @@
+import area
+a=int(input("enter a number"))
+area1=area.area(a)
+perimeter1=area.perimeter(a)
+print(area1)
+print(perimeter1)
